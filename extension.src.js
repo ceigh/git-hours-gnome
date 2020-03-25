3d98731501d897419daff56a2947f82cdfc5f0e5
Artjom Löbsack <ceigh@pm.me>
2020-03-25 20:28:57 +0500

    :fire: Back to just js

---
+++ extension.src.js
@@ -0,0 +1,24 @@
+/* global imports */
+
+const Main = imports.ui.main
+const { St } = imports.gi.St
+
+let label
+
+// eslint-disable-next-line no-unused-vars
+async function init () {
+  label = new St.Label({
+    style_class: 'label',
+    text: 'starting...'
+  })
+}
+
+// eslint-disable-next-line no-unused-vars
+async function enable () {
+  Main.panel._rightBox.insert_child_at_index(label, 1)
+}
+
+// eslint-disable-next-line no-unused-vars
+async function disable () {
+  Main.panel._rightBox.remove_child(label)
+}
