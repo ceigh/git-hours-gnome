e228f9cde5bc386b58ef9dfbc2e2e111c71b13ec
Artjom Löbsack <ceigh@pm.me>
2020-03-25 20:44:03 +0500

    :pencil: Change metadata site

--- extension.src.js
+++ extension.src.js
@@ -6,7 +6,7 @@ const { St } = imports.gi.St
 let label
 
 // eslint-disable-next-line no-unused-vars
-async function init () {
+function init () {
   label = new St.Label({
     style_class: 'label',
     text: 'starting...'
@@ -14,11 +14,11 @@ async function init () {
 }
 
 // eslint-disable-next-line no-unused-vars
-async function enable () {
+function enable () {
   Main.panel._rightBox.insert_child_at_index(label, 1)
 }
 
 // eslint-disable-next-line no-unused-vars
-async function disable () {
+function disable () {
   Main.panel._rightBox.remove_child(label)
 }
