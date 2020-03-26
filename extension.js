2fbdf15192a8bcf3ebb0146272d5570759b5ba57
Artjom Löbsack <ceigh@pm.me>
2020-03-27 00:08:20 +0500

    :recycle: Separate code and handle errors

--- extension.js
+++ extension.js
@@ -1,53 +1,76 @@
-/* global imports */
+// Imports
 const St = imports.gi.St
+const GLib = imports.gi.GLib
 const Main = imports.ui.main
 const MainLoop = imports.mainloop
-const GLib = imports.gi.GLib
+
 let label, timer
 
+
 // Defaults
-// TODO: configuration
-const env = getEnv()
-const path = `${env.HOME}/.local/share/gnome-shell/extensions/git-hours@ceigh`
+const home = GLib.getenv('HOME')
+const interval = getInterval()
+const path = getPath()
 const email = getEmail()
-const interval = 25
 
-function call (command) {
-  const [, out] = GLib.spawn_command_line_sync(command)
-  return out.toString()
+
+// Helpers
+// since runtime didnt convert `out` to string
+function toString(byteArray) {
+  return String.fromCharCode
+    .apply(null, byteArray).replace('\n', '')
 }
 
-function getEnv () {
-  const lines = call('env').split('\n')
-  const entries = lines.map(l => l.split('='))
-  return Object.fromEntries(entries)
+function getInterval() {
+  // temporary
+  return 25 // minutes
 }
 
-function getEmail () {
-  const cfg = call(`cat ${env.HOME}/.gitconfig`)
-  return cfg.split('\n')[1].split(' ')[2]
+function getPath() {
+  // temporary
+  return `${home}/.local/share/gnome-shell/extensions/git-hours@ceigh`
 }
 
-async function getHours () {
-  const total = call(`docker run --rm -v ${path}:/code zaherg/git-hours`)
-  label.set_text(JSON.parse(total)[email].hours.toString())
-  return true
+function getEmail() {
+  const [res, out] = GLib.spawn_command_line_sync('git config user.email')
+  if (!res) return
+
+  const email = toString(out)
+  // log(`User email: ${email}.`)
+  return email
 }
 
-// eslint-disable-next-line no-unused-vars
-function init () {
-  label = new St.Label({ text: '' })
+function getHours() {
+  const command = `docker run --rm -v ${path}:/code zaherg/git-hours`
+  const [res, out] = GLib.spawn_command_line_sync(command)
+  if (!res) return
+
+  const total = toString(out)
+  // log(`Total: ${total}.`)
+  const totalObj = JSON.parse(total)
+
+  const user = totalObj[email]
+  label.set_text(user.hours.toString())
+
+  return true // for timer
+}
+
+
+// Core
+/* eslint-disable-next-line no-unused-vars */
+function init() {
+  label = new St.Label({ text: '...' })
   getHours()
 }
 
-// eslint-disable-next-line no-unused-vars
-function enable () {
+/* eslint-disable-next-line no-unused-vars */
+function enable() {
   Main.panel._rightBox.insert_child_at_index(label, 1)
   timer = MainLoop.timeout_add_seconds(60.0 * interval, getHours)
 }
 
-// eslint-disable-next-line no-unused-vars
-function disable () {
+/* eslint-disable-next-line no-unused-vars */
+function disable() {
   Main.panel._rightBox.remove_child(label)
   MainLoop.source_remove(timer)
 }
