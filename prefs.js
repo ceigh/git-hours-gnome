d2b9dddbb91f9276e41850c2454fe8aed19ad76e
Artjom Löbsack <ceigh@pm.me>
2020-03-30 14:14:05 +0500

    :sparkles: Replace interval field with range

--- prefs.js
+++ prefs.js
@@ -11,7 +11,6 @@ const {
 
 // Vars
 let settings, settingsBox
-let path, email
 
 
 // Helpers
@@ -26,28 +25,48 @@ function generateLabel(text) {
 function addFileChooser() {
   const fileChooserBox = new Gtk.VBox({ border_width: 10 })
   const fileChooserLabel = generateLabel('Path to git repository')
+
+  let path
+  try { path = getPath() } catch { path = '' }
+
   const fileChooser = new Gtk.FileChooserButton({
     title: 'Select git directory',
-    action: 2
+    action: 2,
+    select_multiple: false
   })
+  fileChooser.set_current_folder(path || '/home')
 
   fileChooserBox.pack_start(fileChooserLabel, false, false, 10)
   fileChooserBox.pack_end(fileChooser, false, false, 0)
   settingsBox.pack_start(fileChooserBox, true, false, 0)
 }
 
-
-function addEntry({ placeholder, label, text = '' }) {
+function addEntry() {
   const entryBox = new Gtk.VBox({ border_width: 10 })
-  const entryLabel = generateLabel(label)
+  const entryLabel = generateLabel('Your git email')
+
+  let email
+  try { email = getEmail() } catch { email = '' }
+
   const entry = new Gtk.Entry({
-    placeholder_text: placeholder,
-    text
+    placeholder_text: 'example@example.com',
+    text: email
   })
 
   entryBox.pack_start(entryLabel, false, false, 10)
   entryBox.pack_end(entry, false, false, 0)
   settingsBox.pack_start(entryBox, true, false, 0)
+}
+
+function addRange() {
+  const rangeBox = new Gtk.VBox({ border_width: 10 })
+  const rangeLabel = generateLabel('Interval between checks (in minutes)')
+  const range = Gtk.Scale.new_with_range(0, 5, 120, 5)
+  range.set_value(getInterval())
+
+  rangeBox.pack_start(rangeLabel, false, false, 10)
+  rangeBox.pack_end(range, false, false, 10)
+  settingsBox.pack_start(rangeBox, true, false, 0)
 }
 
 
@@ -60,21 +79,9 @@ function init() {
 
 /* eslint-disable-next-line no-unused-vars */
 function buildPrefsWidget() {
-  try { path = getPath() } catch { path = '' }
-  try { email = getEmail() } catch { email = '' }
-
   addFileChooser()
-  addEntry({
-    placeholder: 'example@example.com',
-    label: 'Your git email',
-    text: email
-  })
-  addEntry({
-    placeholder: '30',
-    label: 'Interval between checks (in minutes)',
-    text: getInterval().toString()
-  })
-
+  addEntry()
+  addRange()
   settingsBox.show_all()
   return settingsBox
 }
